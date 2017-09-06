24bc474c240a5f9f4dfe98f03711fbfee9ff7773
vasil <andr_vasil@mail.ru>
2017-09-06 18:52:26 +0300

    Рабочая версия

--- mavenproject1/src/main/java/rtk/sso/keycloak/model/federatedIdentityRepresentation.java
+++ mavenproject1/src/main/java/rtk/sso/keycloak/model/federatedIdentityRepresentation.java
@@ -10,5 +10,36 @@ package rtk.sso.keycloak.model;
  * @author vasil
  */
 public class federatedIdentityRepresentation {
-    
+
+    private String identityProvider;
+    private String userId;
+    private String userName;
+
+    public federatedIdentityRepresentation() {
+    }
+
+    public String getIdentityProvider() {
+        return identityProvider;
+    }
+
+    public void setIdentityProvider(String identityProvider) {
+        this.identityProvider = identityProvider;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
 }
